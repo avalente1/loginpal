2df0134902a919ac076d66f25862412414fc255c
avalente1 <avalente111@gmail.com>
2013-08-08 17:20:01 -0500

    Took out extra email

--- db/migrate/20130807021838_add_email_to_users.rb
+++ db/migrate/20130807021838_add_email_to_users.rb
@@ -1,6 +1,6 @@
 class AddEmailToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :email, :string
+    #add_column :users, :email, :string
     remove_column :users, :password
   end
 end
