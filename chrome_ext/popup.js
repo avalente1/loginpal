9ec1c15dc7c43196c31c11517befd17875829708
avalente1 <avalente111@gmail.com>
2013-09-05 23:02:27 -0500

    Added basic logic to tell people to go to loginpal.com to add / update content and make sure they are signed in

--- chrome_ext/popup.js
+++ chrome_ext/popup.js
@@ -3,8 +3,11 @@ $.getJSON('http://www.loginpal.com/sites.json', 'limit=150', processWebsites);
 function processWebsites(data) {
 var infoHTML='';
 
+var go_to='Please login or sign up at loginpal.com!';
 
-
+// if (data === nil) {
+  $('#go_to').html(go_to)
+// }
 
 
 $.each(data.sites, function(website, websiteDetails) {
